9bd117b2803bc18daaebef0a1fe3e5e9c52e8f55
Vora Mustafa Makbul <mustafa.vora@saki.iitb.ac.in>
2019-01-22 23:28:56 +0530

    Changed student_compare file

--- student_compare.py
+++ student_compare.py
@@ -1,37 +1,40 @@
 import xml.etree.ElementTree as ET
-import pandas as pd
+#import pandas as pd
+import numpy as np
 
-tree = ET.parse("Early_datasets/agh-fis-spr17.xml")
+tree = ET.parse("Early_datasets/muni-fsps-spr17.xml")
 
 root = tree.getroot()
-print(root)
-n=0
-for students in root.iter('students'):
-    for student in students.iter('student'):
-        n=n+1
-        
-dictionary = dict() #dictionary to store id of students per course
-for students in root.iter('students'):
-    for student in students.iter('student'):
-        student_id = student.attrib['id']
-        for course in student.iter('course'):
-            course_id = course.attrib['id']
-            if course_id in dictionary.keys():
-                dictionary[course_id].append(student_id)
-            else:
-                dictionary[course_id]=[student_id]
-import numpy as np
-stu_common_courses = np.zeros((n,n))#matrix storing number of common courses for pairs of students
-for i in dictionary.keys():
-    for y in dictionary[i]:
-        j=int(y)-1           #index goes for 0 to 1640 for a matrix of size 1641
-        for n in dictionary[i]:
-            k=int(n)-1
-            if(j==k):
-                stu_common_courses[j][k]=0
-            else:
-                stu_common_courses[j][k]=stu_common_courses[j][k]+1
-print(stu_common_courses)
+#print(root)
+def common_courses():
+	n=0
+	for students in root.iter('students'):
+		for student in students.iter('student'):
+		n=n+1
+		
+	dictionary = dict() #dictionary to store id of students per course
+	for students in root.iter('students'):
+		for student in students.iter('student'):
+		student_id = student.attrib['id']
+		for course in student.iter('course'):
+			course_id = course.attrib['id']
+			if course_id in dictionary.keys():
+			dictionary[course_id].append(student_id)
+			else:
+			dictionary[course_id]=[student_id]
+
+	stu_common_courses = np.zeros((n,n))#matrix storing number of common courses for pairs of students
+	for i in dictionary.keys():
+		for y in dictionary[i]:
+		j=int(y)-1           #index goes for 0 to 1640 for a matrix of size 1641
+		for n in dictionary[i]:
+			k=int(n)-1
+			if(j==k):
+			stu_common_courses[j][k]=0
+			else:
+			stu_common_courses[j][k]=stu_common_courses[j][k]+1
+	return stu_common_courses
+#print(stu_common_courses)
 
 
 
